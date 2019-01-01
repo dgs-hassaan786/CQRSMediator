958cab80304760bd89bf74e6af2e1eedf79db2e7
hassaan khan <hassaan.khan@dgsworld.com>
2019-01-01 23:39:50 +0500

    An introduction to .net core. Added an empty project and started configuring it to work with the views and services. Looking forward to implement the domain patterns like Repository and CQRS
    Signed-off-by: hassaan khan <hassaan.khan@dgsworld.com>

---
+++ CorePub.Foundations/Configs/WebConfigurationProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorePub.Foundations.Configs
+{
+    public class WebConfigurationProvider
+    {
+
+    }
+}
---
+++ CorePub/Configurations/Startup/ConfigureMvcExtensions.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CorePub.Configurations.Startup
+{
+    public static partial class ConfigurationExtensions
+    {
+        /// <summary>
+        /// This extension is used for adding the MVC functionality in the application
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection ConfigureMvc(this IServiceCollection services)
+        {
+            //So we have added the MVC in the pipeline along with the options                        
+            services.AddMvc(options =>
+            {
+                //We are globally setting the AutoValidateAntiforgeryTokenAttribute at the runtime
+                options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+            })
+
+            //We are adding the Localization based on the language globally 
+            //via AddViewLocalization() extension
+            //for further understanding check out the article 
+            //https://andrewlock.net/adding-localisation-to-an-asp-net-core-application/
+            .AddViewLocalization()
+
+            //Similarly we can add localization in the Data annotation 
+            //via AddDataAnnotationsLocalization() extension
+            .AddDataAnnotationsLocalization()
+
+            //Inorder to configure the compatibility with the version of mvc
+            //we use the SetCompatibilityVersion() extension
+            //It is available in the package Microsoft.AspNetCore.Mvc.Formatters.Xml
+            //For installation check: https://stackoverflow.com/questions/48546997/how-to-fix-imvcbuilder-doesnt-contain-a-definition-for-addxmldatacontractser
+            .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            return services;
+        }
+
+        /// <summary>
+        /// This extension is used for adding the routing functionality in the MVC application
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder ConfigureRouting(this IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+
+            //Uncomment this line of code if you wish to use the MVC with the default routing
+            //app.UseMvcWithDefaultRoute();
+
+            app.UseMvc(routeConfig =>
+            {
+                routeConfig.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+            return app;
+        }
+    }
+}
